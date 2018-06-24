e1fb50e7628ae8b9eb73de04573b299501c5184f
yaaredry <31453615+yaaredry@users.noreply.github.com>
2018-06-24 19:22:15 +0300

    sort by value

--- run.rb
+++ run.rb
@@ -4,6 +4,7 @@ my_parser = WordCountParser.new("books/")
 
 my_parser.form_dictionary()
 
-my_parser.sort_by("key")
+#my_parser.sort_by("key")
+my_parser.sort_by("value") 
 my_parser.print_dictionary_to_file()
 
